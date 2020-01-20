1748daa8060f82670811482285f9a95977bb9374
Hoang Dang <bdhoang.ht@gmail.com>
2020-01-20 16:02:07 +0700

    update

--- src/routes/controllers/leave.controller.js
+++ src/routes/controllers/leave.controller.js
@@ -7,21 +7,63 @@ const CODE = require('./../helper/response-code');
 const makeResponse = require('./../helper/make-response');
 
 const middleWare = require('./../middlewares');
+const LeaveService = require('../../services/Leave.service');
 
 router.post('/add', middleWare.authenticateRequired, (req, res) => {
-
+  LeaveService.add({user: req.user, options: req.body}).then(rs => {
+    makeResponse(res, jsonResponse(CODE.SUCCESS, CODE[CODE.SUCCESS], rs));
+  }).catch(err => {
+    makeResponse(res, jsonResponse(CODE.GENERIC_ERROR, err.message, {}));
+  })
 });
 
 router.post('/get', middleWare.authenticateRequired, (req, res) => {
-
+  LeaveService.get({user: req.user, options: req.body}).then(rs => {
+    makeResponse(res, jsonResponse(CODE.SUCCESS, CODE[CODE.SUCCESS], rs));
+  }).catch(err => {
+    makeResponse(res, jsonResponse(CODE.GENERIC_ERROR, err.message, {}));
+  })
 });
 
 router.post('/approve', middleWare.authenticateRequired, middleWare.requireManager, (req, res) => {
+  LeaveService.approve({user: req.user, options: req.body}).then(rs => {
+    makeResponse(res, jsonResponse(CODE.SUCCESS, CODE[CODE.SUCCESS], rs));
+  }).catch(err => {
+    makeResponse(res, jsonResponse(CODE.GENERIC_ERROR, err.message, {}));
+  })
+});
 
+router.post('/list-by-user', middleWare.authenticateRequired, (req, res) => {
+  req.body.getAll = false;
+  LeaveService.list({user: req.user, options: req.body}).then(rs => {
+    makeResponse(res, jsonResponse(CODE.SUCCESS, CODE[CODE.SUCCESS], rs));
+  }).catch(err => {
+    makeResponse(res, jsonResponse(CODE.GENERIC_ERROR, err.message, {}));
+  })
 });
 
 router.post('/list', middleWare.authenticateRequired, middleWare.requireManager, (req, res) => {
-
+  req.body.getAll = true;
+  LeaveService.list({user: req.user, options: req.body}).then(rs => {
+    makeResponse(res, jsonResponse(CODE.SUCCESS, CODE[CODE.SUCCESS], rs));
+  }).catch(err => {
+    makeResponse(res, jsonResponse(CODE.GENERIC_ERROR, err.message, {}));
+  })
 });
 
+router.post('/delete', middleWare.authenticateRequired, middleWare.requireManager, (req, res) => {
+  LeaveService.delete({user: req.user, options: req.body}).then(rs => {
+    makeResponse(res, jsonResponse(CODE.SUCCESS, CODE[CODE.SUCCESS], rs));
+  }).catch(err => {
+    makeResponse(res, jsonResponse(CODE.GENERIC_ERROR, err.message, {}));
+  })
+});
+
+router.post('/cancel', middleWare.authenticateRequired, middleWare.requireManager, (req, res) => {
+  LeaveService.cancel({user: req.user, options: req.body}).then(rs => {
+    makeResponse(res, jsonResponse(CODE.SUCCESS, CODE[CODE.SUCCESS], rs));
+  }).catch(err => {
+    makeResponse(res, jsonResponse(CODE.GENERIC_ERROR, err.message, {}));
+  })
+});
 module.exports = router;
---
+++ src/services/Leave.service.js
@@ -0,0 +1,73 @@
+const models = require("../models");
+const LeaveModel = models.Leave;
+const Op = require('sequelize').Op;
+
+module.exports = {
+  add: function (data) {
+    return new Promise((resolve, reject) => {
+      LeaveModel.create({
+        idRequester: data.user.id,
+        startTime: data.options.startTime,
+        endTime: data.options.endTime,
+        reason: data.options.reason,
+        description: data.options.description
+      }).then(resolve).catch(reject)
+    })
+  },
+  get: function (data) {
+    return new Promise((resolve, reject) => {
+      LeaveModel.findByPk(data.options.id).then(lm => {
+        if (lm) {
+          resolve(lm);
+        } else {
+          reject("No record found by id");
+        }
+      }).catch(reject)
+    })
+  },
+  approve: function (data) {
+    return new Promise((resolve, reject) => {
+      LeaveModel.findByPk(data.options.id).then(lm => {
+        if (lm) {
+          lm.status = 'accepted';
+          lm.idApprover = data.user.id;
+          lm.save().then(resolve).catch(reject);
+        } else {
+          reject("No record found by id");
+        }
+      }).catch(reject)
+    })
+  },
+  list: function (data) {
+    return new Promise((resolve, reject) => {
+      let options = data.options.getAll ? {} : {idRequester: data.user.id};
+      LeaveModel.findAll({where: options}).then(resolve);
+    })
+  },
+  cancel: function (data) {
+    return new Promise((resolve, reject) => {
+      LeaveModel.findByPk(data.options.id).then(lm => {
+        if (lm) {
+          lm.status = "rejected";
+          lm.idApprover = data.user.id;
+          lm.save().then(resolve).catch(reject);
+        } else {
+          reject("No record found by id");
+        }
+      }).catch(err => {
+        reject(err);
+      })
+    })
+  },
+  delete: function (data) {
+    return new Promise((resolve, reject) => {
+      LeaveModel.findByPk(data.options.id).then(lm => {
+        if (lm) {
+          lm.destroy().then(resolve).catch(reject)
+        } else {
+          reject("No record found by id");
+        }
+      }).catch(reject)
+    })
+  }
+};
